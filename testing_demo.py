e0ffb481fd0e07936e03ed18421b1a74e9617d1a
Jeroen van Gemert <jeroen.vangemert@kpn.com>
2020-03-15 17:31:17 +0100

    Added all functions on domains

--- testing_demo.py
+++ testing_demo.py
@@ -28,57 +28,17 @@ managed_domains=domains.list_domains()
 print(json.dumps(managed_domains,indent=2))
 print()
 
-managed_domains=domains.list_dns_entries(domain)
-print(json.dumps(managed_domains,indent=2))
-print()
-
-data = '''{
-  "dnsEntry": {
-    "name": "@",
-    "expire": 86400,
-    "type": "A",
-    "content": "37.97.254.27"
-  }
-}'''
-managed_domains=domains.delete_dns_entry(domain, data)
+managed_domains=domains.info_tld('nl')
 print(json.dumps(managed_domains,indent=2))
 print()
 exit()
 
-# Request DNS entries for this domain
-dns_entries = domains.get_dns(domain)
-print(json.dumps(dns_entries,indent=2))
+data = '''{
+  "domainNames": [
+    "gemert.net"
+  ]
+}'''
+managed_domains=domains.domains_availability(data)
+print(json.dumps(managed_domains,indent=2))
 print()
-
-# Find entry
-found_dns_entries = []
-for dns_entry in dns_entries['dnsEntries']:
-  if dns_entry['name'] == find_dns_entry and dns_entry['type'] == 'A':
-    found_dns_entries.append(dns_entry)
-    print (dns_entry)
-print (json.dumps(found_dns_entries))
-print()
-
-# Change entry for this domain with current public IP
-if len(found_dns_entries) == 0:
-  data = '''{
-    "dnsEntry": {
-      "name": "''' + find_dns_entry + '''",
-      "expire": 300,
-      "type": "A",
-      "content": "''' + pub_ip + '''"
-    }
-  }
-  '''
-  domains.post_dns(domain, data)
-elif len(found_dns_entries) == 1:
-  if found_dns_entries[0]['content'] == pub_ip:
-    print('No change needed')
-  else:
-    print('Change dns entry')
-    print('From: ' + json.dumps(found_dns_entries[0]))
-    found_dns_entries[0]['content'] = pub_ip
-    print('To  : ' + json.dumps(found_dns_entries[0]))
-    domains.patch_dns(domain, '{"dnsEntry": ' + json.dumps(found_dns_entries[0]) +'}')
-else:
-  print('Multiple entries found, can\'t determine which to change (if any).')
+exit()
--- transip_api_v6/Domains.py
+++ transip_api_v6/Domains.py
@@ -2,12 +2,13 @@ import requests
 import json
 
 class Domains:
-    base_url='https://api.transip.nl/v6/domains'
+    base_url='https://api.transip.nl/v6'
+    base_url_domains=base_url + '/domains'
     def __init__(self, headers):
         self.headers = headers
 
     def list_domains(self):
-        url = self.base_url
+        url = self.base_url_domains
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             print('Getting domains failed with status code: {0}'.format(res.status_code))
@@ -16,7 +17,7 @@ class Domains:
         return json.loads(res.text)
 
     def retrieve_domain(self, domain):
-        url = self.base_url + '/' + domain
+        url = self.base_url_domains + '/' + domain
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             print('Retrieve domain "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -25,7 +26,7 @@ class Domains:
         return json.loads(res.text)
 
     def register_domain(self, data):
-        url = self.base_url
+        url = self.base_url_domains
         res = requests.post(url, headers=self.headers, data=data)
         if res.status_code != 201:
             print('Creating/Transfering domain failed with status code: {0}'.format(res.status_code))
@@ -37,7 +38,7 @@ class Domains:
         return Domains.register_domain(self, data)
 
     def update_domain(self, data):
-        url = self.base_url
+        url = self.base_url_domains
         res = requests.put(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Creating/Transfering domain failed with status code: {0}'.format(res.status_code))
@@ -50,7 +51,7 @@ class Domains:
             data = '{"endTime": "immediately"}'
         else:
             data = '{"endTime": "end"}'
-        url = self.base_url + '/' + domain
+        url = self.base_url_domains + '/' + domain
         res = requests.delete(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Canceling domain failed with status code: {0}'.format(res.status_code))
@@ -59,7 +60,7 @@ class Domains:
         return
 
     def get_domain_branding(self, domain):
-        url = self.base_url + '/' + domain + '/branding'
+        url = self.base_url_domains + '/' + domain + '/branding'
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             print('Get domain branding for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -68,7 +69,7 @@ class Domains:
         return json.loads(res.text)
 
     def update_domain_branding(self, domain, data):
-        url = self.base_url + '/' + domain + '/branding'
+        url = self.base_url_domains + '/' + domain + '/branding'
         res = requests.put(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Update domain branding for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -77,7 +78,7 @@ class Domains:
         return
 
     def list_contacts(self, domain):
-        url = self.base_url + '/' + domain + '/contacts'
+        url = self.base_url_domains + '/' + domain + '/contacts'
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             print('List domain contacts for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -86,7 +87,7 @@ class Domains:
         return json.loads(res.text)
 
     def update_contacts(self, domain, data):
-        url = self.base_url + '/' + domain + '/contacts'
+        url = self.base_url_domains + '/' + domain + '/contacts'
         res = requests.put(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Update contacts for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -95,7 +96,7 @@ class Domains:
         return
 
     def list_dns_entries(self, domain):
-        url=self.base_url + '/' + domain + '/dns'
+        url=self.base_url_domains + '/' + domain + '/dns'
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             print('Getting dns entries for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -104,7 +105,7 @@ class Domains:
         return json.loads(res.text)
 
     def add_dns_entry(self, domain, data):
-        url = self.base_url + '/' + domain + '/dns'
+        url = self.base_url_domains + '/' + domain + '/dns'
         res = requests.post(url, headers=self.headers, data=data)
         if res.status_code != 201:
             print('Creating dns entry for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -114,7 +115,7 @@ class Domains:
         return
 
     def update_dns_entry(self, domain, data):
-        url = self.base_url + '/' + domain + '/dns'
+        url = self.base_url_domains + '/' + domain + '/dns'
         res = requests.patch(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Updating dns entry for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -124,7 +125,7 @@ class Domains:
         return
 
     def update_all_dns_entries(self, domain, data):
-        url = self.base_url + '/' + domain + '/dns'
+        url = self.base_url_domains + '/' + domain + '/dns'
         res = requests.put(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Updating all dns entries for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -134,7 +135,7 @@ class Domains:
         return
 
     def delete_dns_entry(self, domain, data):
-        url = self.base_url + '/' + domain + '/dns'
+        url = self.base_url_domains + '/' + domain + '/dns'
         res = requests.delete(url, headers=self.headers, data=data)
         if res.status_code != 204:
             print('Deleting dns entry for "{0}" failed with status code: {1}'.format(domain, res.status_code))
@@ -142,3 +143,105 @@ class Domains:
             print('Data: ' + data)
             exit(1)
         return
+
+    def list_dnssec_entries(self, domain):
+        url=self.base_url_domains + '/' + domain + '/dnssec'
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Getting dnssec entries for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def update_all_dnssec_entries(self, domain, data):
+        url = self.base_url_domains + '/' + domain + '/dnssec'
+        res = requests.put(url, headers=self.headers, data=data)
+        if res.status_code != 204:
+            print('Updating all dnssec entries for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            print('Data: ' + data)
+            exit(1)
+        return
+
+    def list_nameservers(self, domain):
+        url=self.base_url_domains + '/' + domain + '/nameservers'
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Getting nameservers for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def update_nameservers(self, domain, data):
+        url = self.base_url_domains + '/' + domain + '/nameservers'
+        res = requests.put(url, headers=self.headers, data=data)
+        if res.status_code != 204:
+            print('Updating nameservers for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            print('Data: ' + data)
+            exit(1)
+        return
+
+    def list_ssl(self, domain):
+        url=self.base_url_domains + '/' + domain + '/ssl'
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Listing ssl for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def whois(self, domain):
+        url=self.base_url_domains + '/' + domain + '/whois'
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Whois for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def whitelabel(self):
+        url=self.base_url + '/whitelabel'
+        res = requests.post(url, headers=self.headers)
+        if res.status_code != 201:
+            print('Whitelabel request failed with status code: {0}'.format(res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def domain_availability(self, domain):
+        url=self.base_url + '/domain-availability/' + domain
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Checking availability for "{0}" failed with status code: {1}'.format(domain, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def domains_availability(self, data):
+        url=self.base_url + '/domain-availability'
+        res = requests.get(url, headers=self.headers, data=data)
+        if res.status_code != 200:
+            print('Checking availability for multiple domains failed with status code: {0}'.format(res.status_code))
+            print(res.text)
+            print('Data: ' + data)
+            exit(1)
+        return json.loads(res.text)
+
+    def list_tlds(self):
+        url=self.base_url + '/tlds'
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Listing tlds failed with status code: {0}'.format(res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
+
+    def info_tld(self, tld):
+        url=self.base_url + '/tlds/.' + tld
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            print('Getting info about tld "{0}" failed with status code: {1}'.format(tld, res.status_code))
+            print(res.text)
+            exit(1)
+        return json.loads(res.text)
